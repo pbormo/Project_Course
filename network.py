289cfc6463706202d5c6822838762977e0c729de
pbormo <107006063+pbormo@users.noreply.github.com>
2025-06-04 22:44:41 +0200

    Update network.py

--- network.py
+++ network.py
@@ -6,7 +6,7 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel
 
 def wait_for_interface(host, interface, timeout=10):
-    """Aspetta che un'interfaccia venga creata per un host"""
+#Aspetta che un'interfaccia venga creata per un host
     start_time = time.time()
     while time.time() - start_time < timeout:
         output = host.cmd(f"ip link show {interface}")
@@ -16,8 +16,8 @@ def wait_for_interface(host, interface, timeout=10):
     return False
 
 def generate_traffic():
-    """Genera traffico tra gli host in modo strutturato"""
-    print("*** Ping tra tutti gli host")
+#Genera traffico tra gli host in modo strutturato
+    print("Ping tra gli host")
     for h in hosts:
         for target in hosts:
             if h != target:
@@ -25,37 +25,37 @@ def generate_traffic():
                 h.cmd(f"ping -c 2 {target.IP()} &")
                 time.sleep(1)
     
-    print("*** Test Iperf da h2 verso tutti gli altri host")
+    print("Test Iperf da h2 verso tutti gli altri host")
     for target in hosts:
         if target != hosts[1]:  # Evita che h2 si test da solo
             print(f"h2 -> {target.name} con iperf")
             hosts[1].cmd(f"iperf -c {target.IP()} -t 3 &")
             time.sleep(1)
     
-    print("*** Traffico TCP su porte specifiche")
+    print("Traffico TCP su porte specifiche")
     hosts[2].cmd("nc -l 5001 &")
     time.sleep(1)
     hosts[4].cmd("echo 'Hello TCP!' | nc 10.0.0.3 5001 &")
     time.sleep(2)
     
-    print("*** Simulazione HTTP da h5 a h7")
+    print("Simulazione HTTP da h5 a h7")
     hosts[4].cmd("wget -O /dev/null http://10.0.0.7 &")
     time.sleep(2)
 
 def setup_network():
     net = Mininet(controller=RemoteController, switch=OVSSwitch)
     
-    print("*** Aggiunta del controller")
+    print("Aggiunta del controller")
     c0 = net.addController('c0')
     
-    print("*** Aggiunta degli switch")
+    print("Aggiunta degli switch")
     s1, s2, s3, s4 = [net.addSwitch(f's{i}') for i in range(1, 5)]
     
-    print("*** Aggiunta degli host")
+    print("Aggiunta degli host")
     global hosts
     hosts = [net.addHost(f'h{i}', ip=f'10.0.0.{i}') for i in range(1, 8)]
     
-    print("*** Creazione dei collegamenti")
+    print("Creazione dei collegamenti")
     for h in hosts[:4]:
         net.addLink(h, s1)
     for h in hosts[4:]:
@@ -65,11 +65,11 @@ def setup_network():
     net.addLink(s2, s3)
     net.addLink(s3, s4)
     
-    print("*** Avvio della rete")
+    print("Avvio della rete")
     net.start()
     time.sleep(5)
     
-    print("*** Avvio della cattura pacchetti su host e switch")
+    print("Avvio della cattura pacchetti su host e switch")
     for h in hosts:
         iface = f"{h.name}-eth0"
         if wait_for_interface(h, iface):
@@ -85,20 +85,20 @@ def setup_network():
                 print(f"Avvio tcpdump su {iface}")
                 os.system(f"sudo tcpdump -i {iface} -w /tmp/{iface}_traffic.pcap &")
         
-    print("*** Generazione del traffico")
+    print("Generazione del traffico")
     generate_traffic()
     time.sleep(10)
     
-    print("*** Stop della cattura pacchetti")
+    print("Stop della cattura pacchetti")
     for h in hosts:
         h.cmd("pkill -x tcpdump")
     for sw in [s1, s2, s3, s4]:
         os.system("sudo pkill -x tcpdump")
     
-    print("*** CLI di Mininet per test aggiuntivi")
+    print("CLI di Mininet per test aggiuntivi")
     CLI(net)
     
-    print("*** Arresto della rete")
+    print("Arresto della rete")
     net.stop()
 
 if __name__ == '__main__':
